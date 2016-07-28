113041cfa6733513e42ebe0e34c9bb47f414c376
Cihangir SAVAS <cihangir@koding.com>
2016-07-28 15:01:32 +0300

    broker: created historical module

---
+++ historical.tf
@@ -0,0 +1,21 @@
+variable "historical_eb_port" {
+  default = "8083"
+}
+
+module "druid_historical_eb" {
+  source               = "./base_eb"
+  app_name             = "${aws_elastic_beanstalk_application.druid.name}"
+  env_name             = "druid-historical"
+  eb_security_groups   = "${module.druid_historical_sg.aws_security_group_eb_id}"
+  elb_security_groups  = "${module.druid_historical_sg.aws_security_group_elb_id}"
+  eb_port              = "${var.historical_eb_port}"
+  elb_port             = "${var.historical_eb_port}"
+  healthCheckEndpoint  = "HTTP:${var.historical_eb_port}/status"
+}
+
+
+module "druid_historical_sg" {
+  source                    = "./base_sg"
+  name                      = "druid-historical"
+  eb_port                   = "${var.historical_eb_port}"
+}
