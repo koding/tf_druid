3a98ca8ba798473d401c9495b457c854cbf8c53c
Cihangir SAVAS <cihangir@koding.com>
2016-07-28 15:01:00 +0300

    broker: created coordinator module

---
+++ coordinator.tf
@@ -0,0 +1,21 @@
+variable "coordinator_eb_port" {
+  default = "8081"
+}
+
+module "druid_coordinator_eb" {
+  source               = "./base_eb"
+  app_name             = "${aws_elastic_beanstalk_application.druid.name}"
+  env_name             = "druid-coordinator"
+  eb_security_groups   = "${module.druid_coordinator_sg.aws_security_group_eb_id}"
+  elb_security_groups  = "${module.druid_coordinator_sg.aws_security_group_elb_id}"
+  eb_port              = "${var.coordinator_eb_port}"
+  elb_port             = "${var.coordinator_eb_port}"
+  healthCheckEndpoint  = "HTTP:${var.coordinator_eb_port}/status"
+}
+
+
+module "druid_coordinator_sg" {
+  source                    = "./base_sg"
+  name                      = "druid-coordinator"
+  eb_port                   = "${var.coordinator_eb_port}"
+}
